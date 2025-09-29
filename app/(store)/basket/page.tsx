05133450b5cc3d53e115632258ff213acf6bed35
Breno Oliveira <brenooliveiraa@gmail.com>
2025-09-29 20:55:18 -0300

    update

--- app/(store)/basket/page.tsx
+++ app/(store)/basket/page.tsx
@@ -9,7 +9,15 @@ import Image from 'next/image';
 import { imageUrl } from '@/lib/imageUrl';
 import Loader from '@/components/Loader';
 import { createMercadoPagoCheckout } from '@/actions/createMercadoPagoCheckout';
-import { Metadata } from '@/actions/createCheckoutSession';
+
+
+
+export type Metadata = {
+  orderNumber: string
+  customerName: string
+  customerEmail: string
+  clerkUserId: string
+}
 
 export default function BasketPage() {
   const groupedItems = useBasketStore((state) => state.getGroupedItems());
